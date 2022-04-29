779da920b510cd7e0f50bb19d81a4c48f96fc274
Sheroo Bhagia <sheroo@sb-mac.lan>
2022-04-29 00:31:06 -0500

    added additional styling on the save button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 var schedule = {};
 var times = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+
+// Using luxon for date and time functionality because Moment is deprecated.  
 var DateTime = luxon.DateTime;
+
+// Add suffix to number on date at top of application
 function getNumberSuffix() {
     num = DateTime.now().day;
     const th = 'th'
@@ -19,24 +23,31 @@ function getNumberSuffix() {
         default: return th
     }
 }
+
+// Renders the page by dynamically creating the row in the app and populating the said rows with data from local storage
 var renderPage = () => {
     schedule = JSON.parse(localStorage.getItem("schedule"));
+    // if nothing in localStorage, create a new object to track schedule items
     if (!schedule) {
         schedule = {};
     }
+
+    // adds date on tops of page
     $("#currentDay").text(`${DateTime.now().weekdayLong}, ${DateTime.now().monthLong} ${DateTime.now().day}${getNumberSuffix()}`);
+
+    // creates a row for each time value in the time variable
     for (var i = 0; i < times.length; i++) {
         var timeRow = $("<div>")
             .addClass("row time-row")
             .attr("id", "timerow-" + (i + 9));
 
+        // creates the children of the above row
         var leftRow = $("<div>")
             .addClass("col-md-1 left-row")
             .text(times[i]);
         if (i === times.length - 1) {
             leftRow.addClass("last-left-row");
         };
-
 
         var midRow = $("<div>")
             .addClass("col-md-10 middle-row")
@@ -46,6 +57,14 @@ var renderPage = () => {
             })
             .text(schedule[i] || "");
 
+        var rightRow = $("<div>")
+            .addClass("col-md-1 right-row oi oi-lock-locked")
+            .attr({
+                id: "rightrow",
+                "data-timerow": i
+            });
+
+        // Sets the color of the rows based on the time of day
         if (DateTime.now().hour > (i + 9)) {
             midRow.css("background-color", "#D7DBDD");
         } else if (DateTime.now().hour === (i + 9)) {
@@ -54,13 +73,8 @@ var renderPage = () => {
         else {
             midRow.css("background-color", "#8FDF95");
         }
-        var rightRow = $("<div>")
-            .addClass("col-md-1 right-row oi oi-lock-locked")
-            .attr({
-                id: "rightrow",
-                "data-timerow": i
-            });
 
+        // appends the newly created row and children to the container
         timeRow.append(leftRow, midRow, rightRow);
         $("#schedule-container").append(timeRow)
     };
@@ -69,12 +83,16 @@ var renderPage = () => {
 
 renderPage();
 
+// listens for a click in the text section of the row to make it editable
 $("#schedule-container").on("click", "#middlerow", function () {
+    // get various attributes of the row that has been clicked
     var text = $(this)
         .text()
         .trim();
     var dataNum = $(this).attr("data-timerow");
     var backgroundColor = $(this).css("background-color");
+
+    // create new input element
     var textInput = $("<textarea>")
         .val(text)
         .addClass("col-md-10 text-row")
@@ -85,9 +103,7 @@ $("#schedule-container").on("click", "#middlerow", function () {
         // get current value of textarea
         var text = $(this).val();
 
-        // get status type and position in the list
-
-        // recreate p element
+        // recreate div element
         var taskP = $("<div>")
             .addClass("col-md-10 middle-row")
             .attr({
@@ -102,6 +118,7 @@ $("#schedule-container").on("click", "#middlerow", function () {
     });
 });
 
+// listens for the save button and saves the text in the row to local storage
 $("#schedule-container").on("click", "#rightrow", function () {
     var row = parseInt($(this).attr("data-timerow"));
     var dataRow = row + 9;
